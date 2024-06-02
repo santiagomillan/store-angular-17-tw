6672682111272036fae668f1dc90bbf0623b6eba
Santiago Millan <santiagomillanpardo@gmail.com>
2024-06-02 01:14:09 -0500

    Mostrando los componentes 29

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { ProductComponent } from './../../components/product/product.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
-export class ListComponent {
-
-}
+export class ListComponent {}
