4c11e8fc906fd523ad9abb6ad2dc1a1fd619c1a7
Santiago Millan <santiagomillanpardo@gmail.com>
2024-06-03 01:55:24 -0500

    Ciclo de vida de componentes: ngOnChanges

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CounterComponent } from './../../../shared/components/counter/counter.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CounterComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
+  duration = signal(1000);
+  message = signal('hola');
 
+  changeDuration(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.duration.set(input.valueAsNumber);
+  }
+  changeMessage(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.message.set(input.value);
+  }
 }
