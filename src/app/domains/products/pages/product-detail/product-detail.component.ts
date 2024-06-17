e274bff54e0df90679e11d1f4ad9247e82581431
Santiago Millan <santiagomillanpardo@gmail.com>
2024-06-16 23:27:01 -0500

    Obteniendo datos del producto 61

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, inject, signal } from '@angular/core';
+import { Product } from '@shared/models/product.model';
+import { ProductService } from '@shared/services/product.service';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
   imports: [],
   templateUrl: './product-detail.component.html',
-  styleUrl: './product-detail.component.css'
+  styleUrl: './product-detail.component.css',
 })
 export class ProductDetailComponent {
+  @Input() id?: string;
+  product = signal<Product | null>(null);
+  private productService = inject(ProductService);
 
+  ngOnInit() {
+    if (this.id) {
+      this.productService.getOne(this.id).subscribe({
+        next: (product) => {
+          console.log(product);
+          this.product.set(product);
+        },
+      });
+    }
+  }
 }
--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -13,4 +13,10 @@ export class ProductService {
   getProducts() {
     return this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products');
   }
+
+  getOne(id: string) {
+    return this.http.get<Product>(
+      `https://api.escuelajs.co/api/v1/products/${id}`
+    );
+  }
 }
