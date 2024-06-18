bea4ab5e25b3b545ae8d92a50a4b9d653b37d12a
Santiago Millan <santiagomillanpardo@gmail.com>
2024-06-17 23:47:50 -0500

    Mostrando categorias desde la API 64

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -5,6 +5,8 @@ import { Product } from '@shared/models/product.model';
 import { HeaderComponent } from '@shared/components/header/header.component';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
+import { Category } from '@shared/models/category.model';
 
 @Component({
   selector: 'app-list',
@@ -17,10 +19,21 @@ export class ListComponent {
   imagenUrl = 'https://picsum.photos/640/640?r=' + Math.random();
 
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
 
   ngOnInit() {
+    this.getProducts();
+    this.getCategories();
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+  }
+
+  private getProducts() {
     this.productService.getProducts().subscribe({
       next: (products) => {
         this.products.set(products);
@@ -31,7 +44,14 @@ export class ListComponent {
     });
   }
 
-  addToCart(product: Product) {
-    this.cartService.addToCart(product);
+  private getCategories() {
+    this.categoryService.getAll().subscribe({
+      next: (data) => {
+        this.categories.set(data);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
   }
 }
