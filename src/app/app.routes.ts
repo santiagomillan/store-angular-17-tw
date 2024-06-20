6755d8bc77a515aa9171e07c2b2f61e1ae09c436
Santiago Millan <santiagomillanpardo@gmail.com>
2024-06-19 23:20:57 -0500

    Aplicando LazyLoading 67

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 
 import { ListComponent } from './domains/products/pages/list/list.component';
-import { AboutComponent } from './domains/info/pages/about/about.component';
+// import { AboutComponent } from './domains/info/pages/about/about.component';
 import { NotFoundComponent } from './domains/info/pages/not-found/not-found.component';
 import { LayoutComponent } from '@shared/components/layout/layout.component';
 
-import { ProductDetailComponent } from '@products/pages/product-detail/product-detail.component';
+// import { ProductDetailComponent } from '@products/pages/product-detail/product-detail.component';
 
 export const routes: Routes = [
   {
@@ -14,15 +14,22 @@ export const routes: Routes = [
     children: [
       {
         path: '',
-        component: ListComponent,
+        loadComponent: () =>
+          import('./domains/products/pages/list/list.component').then(
+            (m) => m.ListComponent
+          ),
       },
       {
         path: 'about',
-        component: AboutComponent,
+        loadComponent: () =>
+          import('./domains/info/pages/about/about.component'),
       },
       {
         path: 'product/:id',
-        component: ProductDetailComponent,
+        loadComponent: () =>
+          import(
+            './domains/products/pages/product-detail/product-detail.component'
+          ),
       },
     ],
   },
